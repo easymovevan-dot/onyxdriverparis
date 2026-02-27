9a00f87609037d2fe2b6c3a3ea7a60af33b28d36
Onyx Drive Paris <easymovevan@gmail.com>
2026-02-28 00:46:20 +0100

    Fix: add missing tailwind postcss module and clean config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
